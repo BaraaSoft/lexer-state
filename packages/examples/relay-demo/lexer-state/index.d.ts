57bf5fdc57f507b6ba1db2d7e8048a1c183be679
BaraaSoft <baraaabuzeid@hotmail.com>
2023-06-20 21:32:41 +1000

    remove examples

--- packages/examples/relay-demo/lexer-state/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './machine';
-export * from './transition';
-export * from './lexem';
-export * from './store';
-export * from './util';
--- packages/examples/relay-demo/lexer-state/store/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ILexerStore {
-    init(): ILexerStore;
-    add<T extends Record<string, any>>(name: string, data: Partial<T>): void;
-    retrieveState(tableName: string): string;
-}
-export * from './relayStore';
