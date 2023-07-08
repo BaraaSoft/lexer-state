abce40ff6c12e98acdf71425b70b5dc322268eb1
Baraa <babuzaid@atlassian.com>
2023-07-08 22:47:18 +1000

    LexerStateHookError

--- packages/machine/src/errors/index.ts
+++ packages/machine/src/errors/index.ts
@@ -1,10 +1,19 @@
 const ErrorMessages = {
   StateError: 'undefined state transition',
+  HookMachineError:
+    'machine is undefined, must initialize useLexerState(machine:?) with a machine',
 } as const;
 
 export class StateError extends Error {
   constructor(message = ErrorMessages.StateError) {
     super(message);
     this.name = 'StateError';
+  }
+}
+
+export class LexerStateHookError extends Error {
+  constructor(message = ErrorMessages.HookMachineError) {
+    super(message);
+    this.name = 'HookMachineError';
   }
 }
